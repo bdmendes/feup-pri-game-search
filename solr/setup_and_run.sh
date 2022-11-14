7f032fe1ba6732ecc285585c5a5250d49da864da
Bruno Mendes <bd_mendes@outlook.com>
2022-11-14 11:52:08 +0000

    Fix life

--- solr/setup_and_run.sh
+++ solr/setup_and_run.sh
@@ -3,5 +3,29 @@ DIR=$(dirname "$0")
 
 cp "$DIR/../data/processed.json" "$DIR/data/processed.json"
 
-docker build $DIR -t steam_games
-docker run -it -p 8983:8983 steam_games
+# Start Solr
+docker stop steam_games
+docker rm steam_games
+docker run -d --name=steam_games -p 8983:8983 solr:8.10
+
+# Wait for Solr to start
+sleep 3
+
+# Delete the core to prevent indexing of the same data multiple times
+docker exec steam_games bin/solr delete -c steam-games
+
+# Create the core
+docker exec steam_games bin/solr create_core -c steam-games
+
+# Schema definition via API
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary @$DIR/data/schema.json \
+    http://localhost:8983/solr/steam-games/schema
+
+# Populate collection via API
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary @$DIR/data/processed.json \
+    http://localhost:8983/solr/steam-games/update?commit=true
+
+echo "Solr is running on http://localhost:8983/solr/#/steam-games/core-overview"
+echo "You can kill this container at anytime with 'docker stop steam_games'"
