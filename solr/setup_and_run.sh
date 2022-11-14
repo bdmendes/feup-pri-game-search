d9d93f199c7f8db65e0223313806357796da62ca
Bruno Mendes <bd_mendes@outlook.com>
2022-11-14 15:24:55 +0000

    Add simple core

--- solr/setup_and_run.sh
+++ solr/setup_and_run.sh
@@ -3,39 +3,48 @@ DIR=$(dirname "$0")
 
 cp "$DIR/../data/processed.json" "$DIR/data/processed.json"
 
+# Kill any existing solr instance
+echo "Killing any existing solr docker instance"
+docker stop solr_steam
+docker rm solr_steam
+
 # Start Solr
+echo ""
 echo "Starting Solr Docker container"
-docker stop steam_games
-docker rm steam_games
-docker run -d --name=steam_games -p 8983:8983 solr:8.10
+docker run -d --name=solr_steam -p 8983:8983 solr:8.10
 
 # Wait for Solr to start
 sleep 2
 
 # Delete the core to prevent indexing of the same data multiple times
 echo ""
-echo "Deleting old core"
-docker exec steam_games bin/solr delete -c steam-games
+echo "Deleting old cores"
+docker exec solr_steam bin/solr delete -c games_simple
+docker exec solr_steam bin/solr delete -c games_tuned
 
-# Create the core
+# Create the simple and tuned cores
 echo ""
 echo "Creating new core"
-docker exec steam_games bin/solr create_core -c steam-games
+docker exec solr_steam bin/solr create_core -c games_simple
+docker exec solr_steam bin/solr create_core -c games_tuned
 
 # Schema definition via API
 echo ""
 echo "Setting up schema"
 curl -X POST -H 'Content-type:application/json' \
     --data-binary @$DIR/data/schema.json \
-    http://localhost:8983/solr/steam-games/schema
+    http://localhost:8983/solr/games_tuned/schema
 
 # Populate collection via API
 echo ""
 echo "Indexing data"
 curl -X POST -H 'Content-type:application/json' \
     --data-binary @$DIR/data/processed.json \
-    http://localhost:8983/solr/steam-games/update?commit=true
+    http://localhost:8983/solr/games_simple/update?commit=true
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary @$DIR/data/processed.json \
+    http://localhost:8983/solr/games_tuned/update?commit=true
 
 echo ""
-echo "Solr is running on http://localhost:8983/solr/#/steam-games/core-overview"
-echo "You can kill this container anytime with 'docker stop steam_games'"
+echo "Solr is running on http://localhost:8983/solr/#/games_tuned/core-overview"
+echo "You can kill this container anytime with 'docker stop solr_steam'"
